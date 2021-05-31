25ba08f641205180dd1c2e4e68b78eddfdd939c9
Vardiak <contact@vardiak.fr>
2021-05-31 18:02:09 +0200

    UPDATE(src/theory/deneigeuse.py): Begin eulerization

--- src/theory/deneigeuse.py
+++ src/theory/deneigeuse.py
@@ -3,20 +3,89 @@ from scipy import sparse
 
 import utils
 
-def get_odd_degree_nodes_directed(G):
+def discard_edge(G, i, j):
+    G[i][j] = 0
+
+def discard_node(G, i):
+    for j in range(len(G)):
+        discard_edge(G, i, j)
+        discard_edge(G, j, i)
+
+def discard_isolated_nodes(G, in_degree, out_degree):
+    def rec_discard_isolated_node(i):
+        if (in_degree[i] > 0) == (out_degree[i] > 0):
+            return
+
+        for j in range(len(G)):
+            if G[i][j] > 0:
+                G[i][j] = 0
+                in_degree[j] -= 1
+                rec_discard_isolated_node(j)
+            elif G[j][i] > 0:
+                G[j][i] = 0
+                out_degree[j] -= 1
+                rec_discard_isolated_node(j)
+
+    for i in range(len(G)):
+        rec_discard_isolated_node(i)
+
+def get_degree_nodes_directed(G):
     n = len(G)
-    degree = [0 for _ in range(n)]
+    in_degree = [0 for _ in range(n)]
+    out_degree = [0 for _ in range(n)]
     for i in range(n):
         for j in range(n):
             if (G[i][j] > 0):
-                degree[j] += 1
-                degree[i] -= 1
-    res = np.nonzero(degree)
-    return res
+                out_degree[i] += 1
+                in_degree[j] += 1
+
+    return in_degree, out_degree
+
+def compute_odd_pairs_directed(graph, in_degree, out_degree):
+
+    odd_pairs = []
+    for i in range(len(graph)):
+        if in_degree[i] >= out_degree[i]:
+            continue
+
+        # noeuds avec surplus de edge sortant
+        to_remove = out_degree[i] - in_degree[i]
+
+        for j in range(len(graph)):
+            if graph[i][j] == 0 or in_degree[j] <= out_degree[j]:
+                continue
+
+            # verifier si le voisin a un surplus de edge entrant
+            discard_edge(graph, i, j)
+            out_degree[i] -= 1
+            in_degree[j] -= 1
+            to_remove -= 1
+            if to_remove == 0:
+                break
+
+    for i in range(len(graph)):
+        if out_degree[i] >= in_degree[i]:
+            continue
+
+        for k in in_degree[i] - out_degree[i]:
+            # find closest j with out_degree > in_degree
+
+            # too much in edges
+            # we need to add v outing edges
+            # breadth first search starting from existsing outing edges
+            pass
+
+        pass
+
+    return odd_pairs
+
 
 def eulerize_graph(G):
-    odd_degree_nodes = get_odd_degree_nodes_directed(G)
-    odd_pairs, all_dist = utils.compute_odd_pairs(G, odd_degree_nodes)
+    in_degree, out_degree = get_degree_nodes_directed(G)
+
+    discard_isolated_nodes(G, in_degree, out_degree)
+
+    odd_pairs, all_dist = compute_odd_pairs_directed(G, in_degree, out_degree)
     
     augmented_path = utils.create_augmented_path(G, odd_pairs, all_dist)
 
@@ -30,7 +99,8 @@ def eulerize_graph(G):
 
 def find_eulerian_circuit(G_aug, augmented_path, start):
     
-    def find_eulerian_circuit_aux(naive_circuit, G_aug, start):
+    naive_circuit = []
+    def find_eulerian_circuit_aux(G_aug, start):
         for i in range(len(G_aug)):
             
             if G_aug[start][i][0] > 0:
@@ -41,15 +111,14 @@ def find_eulerian_circuit(G_aug, augmented_path, start):
                 else:                  # Only one edge
                     G_aug[start][i] = (0, False)
                     G_aug[i][start] = (0, False)
-                find_eulerian_circuit_aux(naive_circuit, G_aug, i)
+                find_eulerian_circuit_aux(G_aug, i)
 
         naive_circuit.append(start)
-        return naive_circuit
 
-    naive_circuit = []
-    naive_circuit =  find_eulerian_circuit_aux(naive_circuit, G_aug, start)
+    find_eulerian_circuit_aux(G_aug, start)
 
     # Replace non-existing path with existing path.
+
     new_circuit = []
     is_already_present = False
     for i in range(len(naive_circuit) - 1):
